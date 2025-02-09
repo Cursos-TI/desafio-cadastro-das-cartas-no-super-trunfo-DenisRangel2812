5c14829cb14a6ccc38ab69f3ce8e5ecea2af79eb
DenisRangel2812 <denisrangel04@gmail.com>
2025-02-09 01:29:17 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,13 +3,32 @@
 int main(){
   printf ("desafio carta super trunfo!\n");
 
-    char estado[01] = "Santa Catarina";
-    char códigodaCarta = 'A';
-    char nome[01] = "Palhoça";
-    int população = 245.477; 
-    float pib = 45.940,71;
-    float área = 394,850;
-    int pontosturísticos = 15;
+    char estado; 
+    char codigo[4];
+    char nomeCidade[50];
+    int populacao; 
+    float area;
+    float pib;
+    int pontosTuristicos;
+} Cidade;
+
+int main() {
+
+  Cidades cidades[4] = {
+{'A', “A01”, “São Paulo”, 12000000, 1521.0, 699000000.0, 30},
+{'A', “A02”, “Rio de Janeiro”, 6700000, 1200.0, 364000000.0, 25},
+{'B', “B01”, “Belo Horizonte”, 2500000, 331.0, 104000000.0, 18},
+{'B', “B02”, “Curitiba”, 1940000, 430.9, 98000000.0, 20},
+
+  };
+printf(“\n=== CIDADES CADASTRADAS === \n”);
+for (int i = 0; i <4; i++) {
+  printf(“\nCidade %d\n”, i + 1);  
+  printf(“
+
+
+
+
 
     
 
